653308637384313b32ae143c5ea672ed13a54382
tetianazakapko <61429075+tetianazakapko@users.noreply.github.com>
2020-03-11 19:59:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+/*Написати програму, яка в консолі буде робити такий шаблон, як права частина трикутника і в рядочку будуть однакові числа.
+1
+22
+333
+4444*/
+
+namespace HomeWork4Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 1; i <= 4; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(" " + i);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+
+
+        
+     }
+}
